f3e1f322545ab9e6d0f9f4cb8d6378a917767e73
cryptoji <kocherginji@gmail.com>
2019-02-14 13:25:35 +0300

    ~ Create react dapp

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,31 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import TokensFlipperContract from "./contracts/TokensFlipper.json";
 import getWeb3 from "./utils/getWeb3";
 
 import "./App.css";
 
 class App extends Component {
-  state = {
-    inputValue: 0,
-    storageValue: 0,
-    web3: null,
-    accounts: null,
-    contract: null
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      // Form
+      createSessionForm: {
+        numberOfParticipants: 20,
+        percentOfWinners: 10,
+        betAmount: 0.1,
+      },
+
+      // State
+      gameSessions: [],
+      numberOfSessions: 0,
+
+      // Constants
+      web3: null,
+      accounts: null,
+      contract: null
+    };
+  }
 
   componentDidMount = async () => {
     try {
@@ -23,20 +37,16 @@ class App extends Component {
 
       // Get the contract instance.
       const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
+      const deployedNetwork = TokensFlipperContract.networks[networkId];
+      const contract = new web3.eth.Contract(
+        TokensFlipperContract.abi,
         deployedNetwork && deployedNetwork.address
       );
 
-      setTimeout(async() => {
-        const response = await this.state.contract.methods.get().call();
-        console.log(response)
-      }, 3000)
-
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance });
+      this.setState({ web3, accounts, contract });
+      this.loadSessions();
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -46,27 +56,82 @@ class App extends Component {
     }
   };
 
-  changeValue = (e) => {
-    this.setState({ inputValue: e.target.value });
+  // -------------------------
+  // Form handlers
+  handleInputChange = (event) => {
+    const { value, name, type } = event.target;
+
+    this.setState(prevState => {
+      return {
+        createSessionForm: {
+          ...prevState.createSessionForm,
+          [name]: type === 'number' ? parseInt(value) : value
+        }
+      };
+    }, () => console.log(this.state.createSessionForm));
   }
 
-  runExample = async (e) => {
-    e.preventDefault();
+  // --------------------------
+  // Utils
+  canJoinToSession = ({ creator, participants }) => {
+    const account = this.state.accounts[0];
+    if(participants.findIndex(address => address === account) >= 0) {
+      return false;
+    }
+    return account !== creator;
+  }
+
+  // -------------------------
+  // Async block chain methods
+  loadSessions = async() => {
+    const { contract } = this.state;
+    const numberOfSessions = await contract.methods.numberOfGameSessions().call();
+    const gameSessions = [];
+
+    for(let i = 0; i < numberOfSessions; i++) {
+      const session = await contract.methods.getSession(i).call();
+      gameSessions.push(session);
+    }
+
+    this.setState({
+      gameSessions,
+      numberOfSessions
+    });
+  }
+
+  joinToSession = async(event, sessionID) => {
+    event.preventDefault();
 
     const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({
+    await contract.methods.joinSession(sessionID).send({
       from: accounts[0],
-      gas: 4712388,
-      gasPrice: 100000000000
+      value: this.state.gameSessions[sessionID].betAmount
     });
 
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
+    this.loadSessions();
+  }
 
-    // Update state with the result.
-    this.setState({ storageValue: response });
+  createGameSession = async(event) => {
+    event.preventDefault();
+
+    const { web3, accounts, contract, createSessionForm } = this.state;
+
+    // Stores a given value, 5 by default.
+    await contract.methods.createSession(
+      web3.utils.toHex(createSessionForm.betAmount*10**18),
+      createSessionForm.percentOfWinners,
+      createSessionForm.numberOfParticipants
+    ).send({
+      from: accounts[0],
+      value: createSessionForm.betAmount*10**18
+    });
+
+    const createdSession = await contract.methods.getSession(this.state.numberOfSessions).call();
+    this.setState(prevState => {
+      return {
+        gameSessions: [...prevState.gameSessions, createdSession]
+      };
+    });
   };
 
   render() {
@@ -75,17 +140,88 @@ class App extends Component {
     }
     return (
       <div className="App">
-        <form onSubmit={this.runExample}>
-          <input
-            type="text"
-            value={this.state.inputValue}
-            onChange={this.changeValue}/>
-          <input
-            type="submit"
-            value="Update"/>
-        </form>
+        <h1>Tokens Flipper</h1>
+        <ul>
+          <li>Wins fund: 1023.3356 ETH</li>
+          <li>Number of sessions: {this.state.numberOfSessions}</li>
+          <li>Number of winners: {this.state.numberOfSessions}</li>
+        </ul>
+        <section>
+          <h3>Create new session</h3>
+          <form onSubmit={this.createGameSession}>
+            <div>
+              <label htmlFor="betAmount">Bet amount: </label>
+              <input
+                type="number"
+                id="betAmount"
+                name="betAmount"
+                value={this.state.createSessionForm.betAmount}
+                onChange={this.handleInputChange}/>
+            </div>
+            <div>
+              <label htmlFor="percentOfWinners">% of winners: </label>
+              <input
+                type="number"
+                id="percentOfWinners"
+                name="percentOfWinners"
+                value={this.state.createSessionForm.percentOfWinners}
+                onChange={this.handleInputChange}/>
+            </div>
+            <div>
+              <label htmlFor="numberOfParticipants">No. of participants: </label>
+              <input
+                type="number"
+                id="numberOfParticipants"
+                name="numberOfParticipants"
+                value={this.state.createSessionForm.numberOfParticipants}
+                onChange={this.handleInputChange}/>
+            </div>
 
-        <strong>The stored value is: {this.state.storageValue}</strong>
+            <input type="submit" value="Create new session"/>
+          </form>
+          <hr/>
+        </section>
+        <section>
+          <h3>Sessions list</h3>
+          {this.state.gameSessions.map((session, index) => {
+            const { web3 } = this.state;
+            return (
+              <div key={index}>
+                <div>
+                  <strong>Bet amount:</strong>
+                  {web3.utils.fromWei(session.betAmount)} ETH
+                </div>
+                <div>
+                  <strong>Number of participants:</strong>
+                  {session.participants.length}/{session.numberOfParticipants}
+                </div>
+                <div>
+                  <strong>Percent of winners:</strong>
+                  {session.percentOfWinners}%
+                </div>
+                <div>
+                  <strong>Creator:</strong>
+                  {session.creator}
+                </div>
+                <div>
+                  <h4>Participants</h4>
+                  {session.participants.map((participant, index) => {
+                    return (<li key={index}>{participant}</li>);
+                  })}
+                </div>
+                {
+                  this.canJoinToSession(session) ?
+                    <button
+                      onClick={(event) => this.joinToSession(event, index)}>
+                      Join to session
+                    </button> :
+                    <p><i>You are joined to this session</i></p>
+                }
+                <hr/>
+              </div>
+            );
+          })}
+        </section>
       </div>
     );
   }

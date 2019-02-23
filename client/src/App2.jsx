98ab553d2e0460270c6569b3519240442ff2f44c
cryptoji <kocherginji@gmail.com>
2019-02-23 22:56:49 +0300

    ~ fix appComponent

--- client/src/App2.jsx
+++ client/src/App2.jsx
@@ -5,28 +5,19 @@ import PropTypes from 'prop-types';
 
 import { Container, Row, Col } from 'reactstrap';
 
-import { getShortAddress } from './utils/index';
-
-import Game from './components/+game/Page';
+import Game from './components/Game';
 import Header from './components/Header';
 import Games from './components/Games';
-import Actions from './components/Actions';
+import Menu from './components/Menu';
 import Events from './components/Events';
+import Statistics from './components/Statistics';
 
-import { fetchGame, fetchGameConfig } from './actions/games';
 import {
   initContract,
   initAccounts,
   initWeb3,
   fetchOwner,
   fetchBalance,
-
-  setWeb3,
-  setOwner,
-  setIsOwner,
-  setContract,
-  setAccounts,
-  setBalance
 } from './actions/blockchain';
 import {
   loadConfigs,
@@ -74,72 +65,18 @@ class AppComponent extends React.Component {
           <Header/>
           <Container fluid={true}>
             {
-              this.props.accounts.length && this.props.contract ?
+              this.props.appIsReady ?
                 (
                   <Row className="mt-3">
-                    {
-                      // Owner access
-                      this.props.isOwner ?
-                        <Col xs="12">
-                          <Actions/>
-                        </Col> : ''
-                    }
                     <Col sm="8" lg="9">
+                      <Menu/>
                       <Route exact path="/" component={Games}/>
                       <Route path="/game/:gameId" component={Game}/>
                     </Col>
+
                     <Col sm="4" lg="3">
-                      <section className="mb-4">
-                        <h5>Statistics</h5>
-                        <ul className="list-unstyled">
-                          <li>
-                            <small className="text-muted">
-                              Current block 2141
-                            </small>
-                            <hr/>
-                          </li>
-                          <li>
-                            <small className="text-muted">
-                              Contract address
-                            </small>
-                            <br/>
-                            <small className="text-primary">
-                              {getShortAddress(this.props.contract.options.address)}
-                            </small>
-                          </li>
-                          <li>
-                            <small className="text-muted">
-                              Contract owner
-                            </small>
-                            <br/>
-                            <small className="text-primary">
-                              {getShortAddress(this.props.owner)}
-                            </small>
-                          </li>
-                          <li>
-                            <small className="text-muted">
-                              Owner reward 3%
-                            </small>
-                          </li>
-                          <li>
-                            <small className="text-muted">
-                              Total games {this.props.gamesCount}
-                            </small>
-                          </li>
-                          <li>
-                            <small className="text-muted">
-                              Total winners N/a
-                            </small>
-                          </li>
-                          <li>
-                            <small className="text-muted">
-                              Total fund N/a ETH
-                            </small>
-                          </li>
-                        </ul>
-                        <hr/>
-                      </section>
-                      {this.props.configs.length ? <Events/> : ''}
+                      <Statistics/>
+                      <Events/>
                     </Col>
                   </Row>
                 ) :
@@ -163,38 +100,18 @@ class AppComponent extends React.Component {
 
 AppComponent.propTypes = {
   // Props
-  web3: PropTypes.object,
-  owner: PropTypes.string,
-  isOwner: PropTypes.bool,
-  contract: PropTypes.object,
-  accounts: PropTypes.array,
-  gamesCount: PropTypes.number,
-  configs: PropTypes.array,
+  appIsReady: PropTypes.bool,
   // Actions
   initWeb3: PropTypes.func.isRequired,
   initAccounts: PropTypes.func.isRequired,
   initContract: PropTypes.func.isRequired,
   fetchOwner: PropTypes.func.isRequired,
   fetchBalance: PropTypes.func.isRequired,
-
-  setWeb3: PropTypes.func.isRequired,
-  setOwner: PropTypes.func.isRequired,
-  setIsOwner: PropTypes.func.isRequired,
-  setContract: PropTypes.func.isRequired,
-  setAccounts: PropTypes.func.isRequired,
-  fetchGame: PropTypes.func.isRequired,
-  fetchGameConfig: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
-  web3: state.blockchain.web3,
-  contract: state.blockchain.contract,
   accounts: state.blockchain.accounts,
-  owner: state.blockchain.owner,
-  isOwner: state.blockchain.isOwner,
-
-  gamesCount: state.games.data.length,
-  configs: state.games.configs
+  appIsReady: !!(state.blockchain.accounts.length && state.blockchain.contract)
 });
 
 const mapDispatchToProps = dispatch => ({
@@ -204,16 +121,7 @@ const mapDispatchToProps = dispatch => ({
   fetchBalance: () => dispatch(fetchBalance()),
   fetchOwner: () => dispatch(fetchOwner()),
   loadConfigs: () => dispatch(loadConfigs()),
-  loadGames: () => dispatch(loadGames()),
-
-  setWeb3: web3 => dispatch(setWeb3(web3)),
-  setOwner: owner => dispatch(setOwner(owner)),
-  setIsOwner: owner => dispatch(setIsOwner(owner)),
-  setContract: contract => dispatch(setContract(contract)),
-  setAccounts: accounts => dispatch(setAccounts(accounts)),
-  setBalance: balance => dispatch(setBalance(balance)),
-  fetchGame: game => dispatch(fetchGame(game)),
-  fetchGameConfig: config => dispatch(fetchGameConfig(config))
+  loadGames: () => dispatch(loadGames())
 });
 
 const App = connect(

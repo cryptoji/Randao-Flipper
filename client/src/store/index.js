c4db2cdf9b9141711bc0b1274aaaeaf7de67e1af
cryptoji <kocherginji@gmail.com>
2019-02-24 22:31:25 +0300

    ~ __REDUX_DEVTOOLS_EXTENSION_COMPOSE__ fixed in app store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,11 +2,10 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk'
 import reducers from '../reducers/index';
 
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   reducers,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composeEnhancer(applyMiddleware(thunk))
 );
 export default store;
